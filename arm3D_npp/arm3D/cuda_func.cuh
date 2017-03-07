c91a167c504628702e4bb136fb181b62f22a1de6
JIN <jinhx@uw.edu>
2017-03-07 11:59:34 -0800

    CHECKPOINT #5: Edge Detect with Laplacian Filter

--- arm3D_npp/arm3D/cuda_func.cuh
+++ arm3D_npp/arm3D/cuda_func.cuh
@@ -1,4 +1,13 @@
 #ifndef CUDAFUNC_CUH
 #define CUDAFUNC_CUH
 
+#include <math.h>
+#include <helper_math.h>
+#include <npp.h>
+
+int divUp(int a, int A);
+
+__device__
+int flatten(int3 i, int3 volSize);
+
 #endif
